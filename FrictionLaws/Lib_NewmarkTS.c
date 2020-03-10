7e33089e19b7e8ff00e2d42986108e855ab84990
J. Nicolas <jorgenhayek@gmail.com>
2020-03-10 17:24:46 +0100

    Separated Files with functions as Lib_*, also, replaced floats by doubles

---
+++ FrictionLaws/Lib_NewmarkTS.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include "Lib_SetOfFrictionLaws.c"
+
+
+void PartialUpScalar(double ScalarIn, double ScalarHalf, double TimeStep, double ScalarDot)
+{
+    ScalarHalf = ScalarIn + 0.5*TimeStep*ScalarDot;
+}
+
+void PartialUpVector(double VectIn[], double VectHalf[], double TimeStep, double VectDot[])
+{
+    VectHalf[0] = VectIn[0] + 0.5*TimeStep*VectDot[0];
+    VectHalf[1] = VectIn[1] + 0.5*TimeStep*VectDot[1];
+}
+
+void CalcSigmaComponent(double Sigma[],double n_i[], double n_j[], double *SigmaScalar)
+{
+    SigmaScalar[0] = Sigma[0]*n_i[0]*n_j[0] + Sigma[1]*n_i[1]*n_j[1] + Sigma[2]*n_i[1]*n_j[0] + Sigma[2]*n_i[0]*n_j[1];
+}
+
+void CompTauCritic(double Sigma[], double Sdot, double Theta, double ListOfParameters[], double n[], double *TauC)
+{
+    double Fric;
+    double SigmaN;
+
+    FricRS(&Fric, Sdot, Theta, ListOfParameters);
+    CalcSigmaComponent(Sigma, n, n, &SigmaN);
+
+    TauC[0] = SigmaN * Fric;
+
+}
+
+void GetFaultTraction(double Sigma[],double n_T[], double n[], double TauC, double *Traction, bool *UpStress)
+{
+    CalcSigmaComponent(Sigma, n_T, n, &Traction);
+    UpStress[0] = false; 
+
+    if (Traction[0] > TauC)
+    {
+        UpStress[0] = true;
+    }  
+}
+
+void GetSlipFromTraction(double delta, double G, bool UpStress, double Traction, double TauC, double OldSlip, double *NewSlip)
+{
+
+    if (UpStress)
+    {
+        NewSlip[0] = OldSlip + (Traction - TauC) * delta / G;
+    } 
+    else
+    {
+        NewSlip[0] = OldSlip;
+    } 
+} 
+
+void GetSlipRate(double OldSlip, double Slip, double TimeStep, double *SlipRate)
+{
+    SlipRate[0] = 2.0 * (Slip - OldSlip) /  TimeStep
+}
