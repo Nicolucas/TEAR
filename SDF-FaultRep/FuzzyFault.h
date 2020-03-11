1495f7f624ac75326ff66cd0a75c0e6d51a737ff
Dave May <dave.mayhem23@gmail.com>
2020-03-11 08:14:44 +0000

    Added makefile * Added header file for function prototypes * Split main() into a separate c file * Compile by typing "make". Remove all object files by typing "make clean"

---
+++ SDF-FaultRep/FuzzyFault.h
@@ -0,0 +1,19 @@
+
+#ifndef __FuzzyFault_h__
+#define __FuzzyFault_h__
+
+/* function prototypes */
+void EvalPhi(double loc[],double* phi);
+void PointInFaut(double distLoc,double delta,bool *InFault);
+void NablaPhi(double loc[],double GradPhi[]);
+void GetProj(double loc[],double GradPhi[], double PhiEval, double Projected[]);
+void GetProjDelta(double loc[],double GradPhi[], double PhiEval, double delta,double Projected[]);
+void GetTwins(double loc[],double PhiEval,double GradPhi[],double Twin[]);
+void TangentVect(double GradPhi[],double TanDir[]);
+void TangentVel(double Velocity[],double TanVect[],double TanVel[]);
+void EvalVelocity(double loc[],double Velocity[]);
+void CalcSlipRate(double VelPlus[], double VelMinus[],double PhiPlus,double PhiMinus, bool LocInFault, double SlipRate[]);
+void LocateInFault(double loc[], bool LocInFault,double GradPhi[], double PhiEval,double delta, double PostLocation[]);
+void EvaluateSlipRateAtPoint(double coor[],double delta,double *sdot);
+
+#endif
