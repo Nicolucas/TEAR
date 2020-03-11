4f8f787ef52e397f08134cace0f217d52346c6fd
Dave May <dave.mayhem23@gmail.com>
2020-03-11 08:03:27 +0000

    Enhance functionality: * Generalise GradPhi() to accept coordinate as first arg * Added method EvaluateSlipRateAtPoint() to collect functionality into a single function

--- SDF-FaultRep/FuzzyFault.c
+++ SDF-FaultRep/FuzzyFault.c
@@ -5,7 +5,7 @@
 /* function prototypes */
 void EvalPhi(double loc[],double* phi);
 void PointInFaut(double distLoc,double delta,bool *InFault);
-void NablaPhi(double GradPhi[]);
+void NablaPhi(double loc[],double GradPhi[]);
 void GetProj(double loc[],double GradPhi[], double PhiEval, double Projected[]);
 void GetProjDelta(double loc[],double GradPhi[], double PhiEval, double delta,double Projected[]);
 void GetTwins(double loc[],double PhiEval,double GradPhi[],double Twin[]);
@@ -14,7 +14,7 @@ void TangentVel(double Velocity[],double TanVect[],double TanVel[]);
 void EvalVelocity(double loc[],double Velocity[]);
 void CalcSlipRate(double VelPlus[], double VelMinus[],double PhiPlus,double PhiMinus, bool LocInFault, double SlipRate[]);
 void LocateInFault(double loc[], bool LocInFault,double GradPhi[], double PhiEval,double delta, double PostLocation[]);
-
+void EvaluateSlipRateAtPoint(double coor[],double delta,double *sdot);
 
 int main (int nargs, char *args[])
 {
@@ -53,7 +53,7 @@ int main (int nargs, char *args[])
     PointInFaut(Phi, delta, &InFault);
     printf("Is point in the fault? : %s\n", InFault ? "true": "false");
 
-    NablaPhi(GradPhi);
+    NablaPhi(loc,GradPhi);
     printf("GradPhi direction is : %f, %f\n", GradPhi[0], GradPhi[1]);
 
     GetProj(loc, GradPhi, Phi, ProjectedLoc);
@@ -88,6 +88,14 @@ int main (int nargs, char *args[])
 
     CalcSlipRate(VelTanOne, VelTanTwo, PhiPL, PhiTwin, InFault, SlipRate);
     printf("Slip rate is : %f, %f\n", SlipRate[0], SlipRate[1]);
+  
+  {
+    double sdot;
+    
+    EvaluateSlipRateAtPoint(loc,delta,&sdot);
+    printf("Slip rate is : %f \n",sdot);
+  }
+
     return(0);
 }
 
@@ -108,7 +116,7 @@ void PointInFaut(double distLoc, double delta, bool *InFault)
 }
 
 /* Calculates GradPhi and returns an array with direction, normalized by def. */
-void NablaPhi(double GradPhi[])
+void NablaPhi(double loc[],double GradPhi[])
 {
     GradPhi[0] = 0.0; /* as in example */
     GradPhi[1] = 1.0;
@@ -214,3 +222,71 @@ void CalcSlipRate(double VelPlus[], double VelMinus[], double PhiPlus, double Ph
     }
 }
 
+
+void NormalVecGetTangentVec(double n[], double t[])
+{
+  t[0] =  n[1];
+  t[1] = -n[0];
+}
+
+void VecDot(double x[], double y[], double *d)
+{
+  *d = x[0]*y[0] + x[1]*y[1];
+}
+
+void VecMag(double x[], double *m)
+{
+  *m = sqrt(x[0]*x[0] + x[1]*x[1]);
+}
+
+
+void EvaluateSlipRateAtPoint(double coor[],double delta,double *sdot)
+{
+  double coor_plus[] = {0,0};
+  double coor_minus[] = {0,0};
+  double vel_plus[] = {0,0};
+  double vel_minus[] = {0,0};
+  double normal[] = {0,0},tangent[] = {0,0};
+  double gradphi[2],phi_p,f_phi_p;
+  double vdott_plus,vdott_minus;
+  
+  sdot[0] = sdot[1] = 0.0;
+  
+  
+  EvalPhi(coor,&phi_p);
+  f_phi_p = fabs(phi_p);
+  
+  /* early return if not inside the fault */
+  if (f_phi_p > delta) { return; }
+  
+  NablaPhi(coor,gradphi);
+  {
+    double mag;
+    
+    VecMag(gradphi,&mag);
+    normal[0] = gradphi[0] / mag;
+    normal[1] = gradphi[1] / mag;
+  }
+  NormalVecGetTangentVec(normal,tangent);
+  
+
+  /* project to phi = 0 */
+  coor_plus[0] = coor[0] - phi_p * normal[0];
+  coor_plus[1] = coor[1] - phi_p * normal[1];
+  
+  /* project to phi = delta */
+  coor_plus[0] = coor_plus[0] + delta * normal[0];
+  coor_plus[1] = coor_plus[1] + delta * normal[1];
+
+  /* project to phi = -delta */
+  coor_minus[0] = coor_plus[0] - 2.0 * delta * normal[0];
+  coor_minus[1] = coor_plus[1] - 2.0 * delta * normal[1];
+
+  EvalVelocity(coor_plus,vel_plus);
+  EvalVelocity(coor_minus,vel_minus);
+  
+  VecDot(vel_plus,tangent,&vdott_plus);
+  VecDot(vel_minus,tangent,&vdott_minus);
+  
+  *sdot = vdott_plus - vdott_minus;
+}
