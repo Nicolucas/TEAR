a2779d10fc12f2582a2d3feead2cd85de2111bb6
J. Nicolas <jorgenhayek@gmail.com>
2020-03-16 01:42:42 +0100

    Fixed distance value for the x-axis of the profile plot

--- ProfilePicking/ProfilePlotterTest.py
+++ ProfilePicking/ProfilePlotterTest.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt 
 import scipy.ndimage
-
+import math
 
 
 def onclick(event):
@@ -31,12 +31,13 @@ fig, ax = plt.subplots(nrows=1)
 
 ax.imshow(Z, origin="lower",cmap="RdBu_r")
 cid = fig.canvas.mpl_connect("button_press_event", onclick)
-print(coords)
 plt.show()
 
 num = 1000
 x, y = np.linspace(coords[0][0], coords[1][0], num), np.linspace(coords[0][1], coords[1][1], num)
 zi = scipy.ndimage.map_coordinates(Z, np.vstack((x,y)))
+Dist = math.sqrt((coords[1][0]- coords[0][0])**2.0+(coords[1][1]- coords[0][1])**2.0)
+ZDist = np.linspace(0, Dist, num)
 
 fig, ax = plt.subplots(nrows=2)
 
@@ -44,6 +45,6 @@ ax[0].imshow(Z,origin="lower",cmap="RdBu_r")
 ax[0].plot([coords[0][0], coords[1][0]], [coords[0][1], coords[1][1]], 'ro-')
 ax[0].axis('image')
 
-ax[1].plot(zi)
+ax[1].plot(ZDist,zi)
 plt.show()
 
