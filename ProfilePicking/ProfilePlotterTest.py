f101762208718a6ec95baa06ec2018abb76c9719
J. Nicolas <jorgenhayek@gmail.com>
2020-03-17 16:38:44 +0100

    Generalized the plotting tool as a class that takes an MGrid  and  the selection of two points to plot transect

--- ProfilePicking/ProfilePlotterTest.py
+++ ProfilePicking/ProfilePlotterTest.py
@@ -4,19 +4,52 @@ import scipy.ndimage
 import math
 
 
-def onclick(event):
-    global ix, iy
-    ix,iy = event.xdata, event.ydata
-    
-    print ('x = {}, y = {}'.format(ix,iy))
-    global coords
+class MGridData:
+    def __init__(self, ZGrid, NumPoints):
+        self.coords = []
+        self.ZGrid = ZGrid
+        self.NumPoints = NumPoints
+        self.fig = None
+        self.cid = None
 
-    coords.append((ix,iy))
 
-    if len(coords) == 2:
-        fig.canvas.mpl_disconnect(cid)
-        plt.close(fig)
-    return coords
+    def onclick(self, event):
+        ix,iy = event.xdata, event.ydata
+        
+        print ('x = {}, y = {}'.format(ix,iy))
+
+        self.coords.append((ix,iy))
+        if len(self.coords) == 2:
+            self.fig.canvas.mpl_disconnect(self.cid)
+            plt.close(self.fig)
+
+
+    def PlotNExtractProfile(self):
+        fig, ax = plt.subplots(nrows=1)
+        self.fig = fig
+        ax.imshow(self.ZGrid, origin="lower",cmap="RdBu_r")
+        cid = self.fig.canvas.mpl_connect("button_press_event", self.onclick)
+        self.cid= cid
+        
+        plt.show()
+
+        x, y = np.linspace(self.coords[0][0], self.coords[1][0], self.NumPoints), \
+               np.linspace(self.coords[0][1], self.coords[1][1], self.NumPoints)
+
+        zi = scipy.ndimage.map_coordinates(Z, np.vstack((x,y)))
+        Dist = math.sqrt((self.coords[1][0] - self.coords[0][0])**2.0 + (self.coords[1][1] - self.coords[0][1])**2.0)
+        ZDist = np.linspace(0, Dist, self.NumPoints)
+
+        fig, ax = plt.subplots(nrows=2)
+        self.fig = fig
+        self.cid = None
+
+        ax[0].imshow(self.ZGrid,origin="lower",cmap="RdBu_r")
+        ax[0].plot([self.coords[0][0], self.coords[1][0]], [self.coords[0][1], self.coords[1][1]], 'ro-')
+
+        ax[1].plot(ZDist,zi)
+        plt.show()
+
 
 N = 100
 X, Y = np.mgrid[-3:3:complex(0, N), -2:2:complex(0, N)]
@@ -24,27 +57,7 @@ Z1 = np.exp(-X**2 - Y**2)
 Z2 = np.exp(-(X - 1)**2 - (Y - 1)**2)
 Z = (Z1 - Z2) * 2
 
-coords = []
 
-
-fig, ax = plt.subplots(nrows=1)
-
-ax.imshow(Z, origin="lower",cmap="RdBu_r")
-cid = fig.canvas.mpl_connect("button_press_event", onclick)
-plt.show()
-
-num = 1000
-x, y = np.linspace(coords[0][0], coords[1][0], num), np.linspace(coords[0][1], coords[1][1], num)
-zi = scipy.ndimage.map_coordinates(Z, np.vstack((x,y)))
-Dist = math.sqrt((coords[1][0]- coords[0][0])**2.0+(coords[1][1]- coords[0][1])**2.0)
-ZDist = np.linspace(0, Dist, num)
-
-fig, ax = plt.subplots(nrows=2)
-
-ax[0].imshow(Z,origin="lower",cmap="RdBu_r")
-ax[0].plot([coords[0][0], coords[1][0]], [coords[0][1], coords[1][1]], 'ro-')
-ax[0].axis('image')
-
-ax[1].plot(ZDist,zi)
-plt.show()
-
+MGrid = MGridData(NumPoints=1000,ZGrid=Z)
+MGrid.PlotNExtractProfile()
+print(MGrid.coords)
