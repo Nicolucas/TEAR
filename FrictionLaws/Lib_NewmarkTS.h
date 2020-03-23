1c5df5ef04ab84c0b937511d8243cb1f92463d10
J. Nicolas <jorgenhayek@gmail.com>
2020-03-23 16:52:54 +0100

    Fixed *.h to extract the friction from the func

--- FrictionLaws/Lib_NewmarkTS.h
+++ FrictionLaws/Lib_NewmarkTS.h
@@ -7,7 +7,7 @@
 void PartialUpScalar(double ScalarIn, double *ScalarHalf, double TimeStep, double ScalarDot);
 void PartialUpVector(double VectIn[], double VectHalf[], double TimeStep, double VectDot[]);
 void CalcSigmaComponent(double Sigma[],double n_i[], double n_j[], double *SigmaScalar);
-void CompTauCritic(double Sigma[], double Sdot, double Theta, double ListOfParameters[], double n[], double *TauC);
+void CompTauCritic(double Sigma[], double Sdot, double Theta, double ListOfParameters[], double n[], double *TauC, double *Friction);
 void GetFaultTraction(double Sigma[],double n_T[], double n[], double TauC, double *Traction, bool *UpStress);
 void GetSlipFromTraction(double delta, double G, bool UpStress, double Traction, double TauC, double OldSlip, double *NewSlip);
 void GetSlipRate(double OldSlip, double Slip, double TimeStep, double *SlipRate);
