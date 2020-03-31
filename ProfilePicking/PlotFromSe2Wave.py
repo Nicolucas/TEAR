866fba48ec77e5d5532e1c1866c2b464b43b7910
J. Nicolas <jorgenhayek@gmail.com>
2020-03-31 22:02:43 +0200

    Fixed dependency for nx and ny

--- ProfilePicking/PlotFromSe2Wave.py
+++ ProfilePicking/PlotFromSe2Wave.py
@@ -15,8 +15,8 @@ se2_coor = se2wave_load_coordinates(filename);
 w_filename = os.path.join(path,"step-1400_wavefield.pbin")
 se2_field = se2wave_load_wavefield(w_filename,True,True);
 
-LCoorX, LCoorY = SeparateList(se2_coor['coor'], se2_coor)
-LFieldX, LFieldY = SeparateList(se2_field['displ'], se2_field)
+LCoorX, LCoorY = SeparateList(se2_coor['coor'], se2_coor['nx'].item(), se2_coor['ny'].item())
+LFieldX, LFieldY = SeparateList(se2_field['displ'], se2_field['nx'].item(), se2_field['ny'].item())
 
 SplineFunction = [RectBivariateSpline(LCoorX[:,0], LCoorY[0,:], LFieldX), 
                   RectBivariateSpline(LCoorX[:,0], LCoorY[0,:], LFieldY)]
